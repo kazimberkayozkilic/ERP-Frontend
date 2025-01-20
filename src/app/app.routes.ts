27d35046ef47170173ba4ce286f2fb6cd2adfbd9
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-21 01:05:55 +0300

    feat: Configured routes for login, layouts and homepage components. - Added route to `“login”` path for `LoginComponent`. - Configured default layout structure for root path `“”` with `LayoutsComponent` as parent and `HomeComponent` as child.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { LayoutsComponent } from './component/layouts/layouts.component';
+import { HomeComponent } from './component/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "",
+    component: LayoutsComponent,
+    children: [
+      {
+        path:"",
+        component: HomeComponent
+      }
+    ]
+  },
+];
