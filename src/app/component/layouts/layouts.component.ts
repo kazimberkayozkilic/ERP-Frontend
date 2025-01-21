c25cdf2ab5111f3ff3cfec0d3f29d85765597f3e
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-21 03:16:22 +0300

    feat: Split the layout into main sidebar, control sidebar, footer and navbar components. - I divided the layout into different components: main sidebar, control sidebar, footer and navbar. - Created corresponding Angular components for each section. - Updated the HTML to include the new component structure for better modularity.

--- src/app/component/layouts/layouts.component.ts
+++ src/app/component/layouts/layouts.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
 
 @Component({
   selector: 'app-layouts',
-  imports: [],
+  imports: [RouterOutlet, NavbarComponent, MainSidebarComponent, FooterComponent, ControlSidebarComponent],
   templateUrl: './layouts.component.html',
   styleUrl: './layouts.component.css'
 })
