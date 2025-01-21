9ce6a681b718e0d87ef4314cc9208244465507f1
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-22 01:31:39 +0300

    feat: Refactored the home widget to use empty and section widgets. - Moved content from the homepage widget to the blank and section widgets. - Used `<app-blank>` to wrap and structure the layout. - Integrated `<app-section>` for section specific content within the blank widget. - Improved component modularity and separation of concerns.

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { BlankComponent } from '../blank/blank.component';
+import { SectionComponent } from '../section/section.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [BlankComponent, SectionComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
