76626fa405c70a794ebc40a4a3edc406f311a0f3
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-23 02:34:24 +0300

    feat: Improved sidebar with active route highlighting and improved styling. - Added `routerLinkActive=“active”` and `[routerLinkActiveOptions]=“{exact:true}”` to dynamically highlight active menu items. - Updated submenu icon style by adding `me-2` for improved spacing.

--- src/app/component/layouts/main-sidebar/main-sidebar.component.ts
+++ src/app/component/layouts/main-sidebar/main-sidebar.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Menus } from '../../../menu';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './main-sidebar.component.html',
   styleUrls: ['./main-sidebar.component.css']
 })
