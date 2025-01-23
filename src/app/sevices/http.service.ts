9cb531a7b5528602f6bf379af78ea243b275c537
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-23 04:11:49 +0300

    feat: Added HttpService with post method and constants for API URL. - Implemented `post` method in `HttpService` to handle HTTP POST requests with authorization headers. - Added callback and error callback functions to handle API responses and errors. - Created `constants.ts` to store the base API URL in the `api` constant for easy configuration.

--- src/app/sevices/http.service.ts
+++ src/app/sevices/http.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { api } from '../constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  post<T>(apiUrl:string, body:any, callBack:(res:T)=>void, errorCallBack?:()=> void){
+    this.http.post<T>(`${api}/${apiUrl}`, body,{
+      headers: {
+        "Authorization": "Bearer " + "token"
+      }
+    }).subscribe({
+        next: (res)=> {
+          callBack(res);
+        },
+        error: (err:HttpErrorResponse)=>{
+          if(errorCallBack){
+            errorCallBack();
+          }
+        }
+      })
+  }
 }
