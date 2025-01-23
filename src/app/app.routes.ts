62194ce109889ab3e60f08949241fac62b4aed53
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-23 04:59:39 +0300

    feat: Added authentication protection for child routes in routes.ts. - Added `canActivateChild` protection to protect child routes under `LayoutsComponent`. - Used `AuthServiceService` to provide authentication before accessing sub-routes. - Redirected unauthenticated users to the login page by applying the `isAuthenticated` check.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { LayoutsComponent } from './component/layouts/layouts.component';
 import { HomeComponent } from './component/home/home.component';
+import { AuthServiceService } from './sevices/auth-service.service';
+import { inject } from '@angular/core';
 
 export const routes: Routes = [
   {
@@ -11,6 +13,7 @@ export const routes: Routes = [
   {
     path: "",
     component: LayoutsComponent,
+    canActivateChild: [()=> inject(AuthServiceService).isAuthenticated()],
     children: [
       {
         path:"",
