f769d541b5e3e064fd5458f74bd917e6211cc746
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-23 04:53:47 +0300

    feat: Implemented token-based authentication in AuthServiceService. - Added `isAuthenticated` method to check token validity and decode JWT. - Used `jwt-decode` to decode the token and extract user information (ID, name, email, username). - Implemented token expiration check to redirect to the login page if the token has expired.

--- src/app/sevices/auth-service.service.ts
+++ src/app/sevices/auth-service.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { jwtDecode, JwtPayload } from 'jwt-decode';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthServiceService {
+  token: string = '';
+  user: any;
+  constructor(
+    private router: Router,
+  ) {}
 
-  constructor() { }
+  isAuthenticated() {
+    this.token = localStorage.getItem('token') ?? '';
+    const decode: JwtPayload | any = jwtDecode(this.token);
+    const exp = decode.exp;
+    const now = new Date().getTime() / 1000;
+
+    if(now > exp){
+      this.router.navigateByUrl("/login");
+      return false;
+    }
+
+    this.user.id = decode["Id"];
+    this.user.name = decode["Name"];
+    this.user.email = decode["Email"];
+    this.user.userName = decode["userName"];
+
+    return true;
+  }
 }
