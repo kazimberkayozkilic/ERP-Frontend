b5b29ca59cd7183871ab1ecbeb2f1a589f82e910
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-25 04:00:33 +0300

    feat: Added functionality to fetch and display customers. - Implemented `getAllCustomers()` method in the component to retrieve customer data using HTTP service. - The `getAllCustomers()` method was integrated with the customers table in the HTML template.

--- src/app/component/customers/customers.component.ts
+++ src/app/component/customers/customers.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../modules/shared.module';
+import { CustomerModel } from '../../models/customer.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-customers',
   standalone: true,
   imports: [SharedModule],
   templateUrl: './customers.component.html',
-  styleUrl: './customers.component.css'
+  styleUrl: './customers.component.css',
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit {
+  customers: CustomerModel[] = [];
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.getAllCustomers();
+  }
+
+  getAllCustomers() {
+    this.http.post<CustomerModel[]>("Customers/GetAll", {}).subscribe((res) => {
+      this.customers = res;
+    });
+  }
 }
