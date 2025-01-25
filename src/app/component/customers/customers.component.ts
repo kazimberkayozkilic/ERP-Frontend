845a1ddf9e53b2ae1639fd3f65ef9b58a74c0f4c
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-25 04:25:31 +0300

    feat: Integrated CustomerPipe into CustomersComponent for filtering. - Added `search: string = “”;` property to `CustomersComponent` to process search input. - Imported `CustomerPipe` into `CustomersComponent` to enable dynamic filtering of customer data. - The HTML template has been updated to include search functionality, allowing users to interactively filter customers based on the search provided.

--- src/app/component/customers/customers.component.ts
+++ src/app/component/customers/customers.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../modules/shared.module';
 import { CustomerModel } from '../../models/customer.model';
 import { HttpClient } from '@angular/common/http';
+import { CustomerPipe } from '../../pipes/customer.pipe';
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, CustomerPipe],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css',
 })
 export class CustomersComponent implements OnInit {
   customers: CustomerModel[] = [];
+  search: string="";
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
