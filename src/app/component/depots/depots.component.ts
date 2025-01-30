a93199ba51e4c66f0c89dc63a45bed9dcd451bab
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-31 02:31:13 +0300

    feat: Implemented the `deleteByIdDepo` method for warehouse deletion. - Added `deleteByIdDepo` method to delete repositories by id. - SweetAlert has been integrated to confirm the deletion with a modal prompt. - After a successful deletion, the repository list is refreshed and a toast notification is displayed.

--- src/app/component/depots/depots.component.ts
+++ src/app/component/depots/depots.component.ts
@@ -3,6 +3,7 @@ import { SharedModule } from '../../modules/shared.module';
 import { DepotModel } from '../../models/depot.model';
 import { HttpService } from '../../sevices/http.service';
 import { DepotPipe } from '../../pipes/depot.pipe';
+import { SwalService } from '../../sevices/swal.service';
 
 @Component({
   selector: 'app-depots',
@@ -15,7 +16,7 @@ export class DepotsComponent implements OnInit {
   depots: DepotModel[] = [];
   search: string = '';
 
-  constructor(private http: HttpService) {}
+  constructor(private http: HttpService, private swal: SwalService) {}
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
@@ -25,5 +26,14 @@ export class DepotsComponent implements OnInit {
       this.depots = res;
     });
   }
+
+  deleteByIdDepo(model: DepotModel){
+      this.swal.callSwal("Depo Sil", `${model.name}`,() => {
+        this.http.post<string>("Depots/DeleteById", {id:model.id}, (res)=>{
+          this.getAllDepots();
+          this.swal.callToast(res, "info");
+        })
+      });
+    }
 
 }
