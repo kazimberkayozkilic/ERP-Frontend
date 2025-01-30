cacc2f1df78e9adc5f917ad4e955c6b93971b79f
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-31 02:26:58 +0300

    feat: Add search functionality and integrated `DepotPipe`. - A string variable `search` has been introduced to filter repositories. - Imported and implemented `DepotPipe` to filter repository data by search term. - Search functionality has been integrated into HTML for dynamic filtering.

--- src/app/component/depots/depots.component.ts
+++ src/app/component/depots/depots.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../modules/shared.module';
 import { DepotModel } from '../../models/depot.model';
 import { HttpService } from '../../sevices/http.service';
+import { DepotPipe } from '../../pipes/depot.pipe';
 
 @Component({
   selector: 'app-depots',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, DepotPipe],
   templateUrl: './depots.component.html',
   styleUrl: './depots.component.css'
 })
 export class DepotsComponent implements OnInit {
   depots: DepotModel[] = [];
+  search: string = '';
 
   constructor(private http: HttpService) {}
   ngOnInit(): void {
