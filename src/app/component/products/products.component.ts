8069af1cb62d3521c9ed6157c1d602d0947ec6e6
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-02 03:28:20 +0300

    feat: Implemented getAllProducts function and search function. - Created `getAllProducts()` to retrieve products from the backend via HTTP. - Implemented search function to filter products by name and type. - Search input integrated into HTML template for dynamic filtering.

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../modules/shared.module';
 import { ProductPipe } from '../../pipes/product.pipe';
+import { ProductModel } from '../../models/product.model';
+import { HttpService } from '../../sevices/http.service';
+import { SwalService } from '../../sevices/swal.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, ProductPipe],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: ProductModel[] = [];
+  search: string="";
 
+
+  constructor(private http: HttpService, private Swal: SwalService) {}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+      this.http.post<ProductModel[]>('Products/GetAll', {}, (res) => {
+        this.products = res;
+      });
+    }
 }
