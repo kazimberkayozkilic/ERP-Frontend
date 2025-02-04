a0895c2305c2de0da37ad1acbe8a67cfd721ec9d
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-05 01:11:17 +0300

    feat: Implemented getAllRecipes() function and integrate search functionality in HTML.

--- src/app/component/recipes/recipes.component.ts
+++ src/app/component/recipes/recipes.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../modules/shared.module';
 import { RecipePipe } from '../../pipes/recipe.pipe';
+import { RecipeModel } from '../../models/recipe.model';
+import { ProductModel } from '../../models/product.model';
+import { HttpService } from '../../sevices/http.service';
+import { SwalService } from '../../sevices/swal.service';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, RecipePipe],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css'
 })
-export class RecipesComponent {
+export class RecipesComponent implements OnInit {
+recipes: RecipeModel[] =[];
+search: string ="";
+products: ProductModel[] = [];
 
+constructor(private http: HttpService, private swal: SwalService){}
+
+
+  ngOnInit(): void {
+    this.getAllRecipes();
+  }
+
+  getAllRecipes() {
+    this.http.post<RecipeModel[]>('Recipes/GetAll', {}, (res) => {
+      this.recipes = res;
+    });
+  }
 }
