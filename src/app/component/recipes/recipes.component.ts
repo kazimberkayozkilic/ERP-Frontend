3addf0dff045def3de7c4b026a1650f0f117a3b0
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-05 00:53:25 +0300

    feat: Completed static structure for the Recipes HTML page.

--- src/app/component/recipes/recipes.component.ts
+++ src/app/component/recipes/recipes.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../modules/shared.module';
+import { RecipePipe } from '../../pipes/recipe.pipe';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css'
 })
