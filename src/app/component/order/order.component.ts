975cd3787f2dc15762ac28efa6cbc5fc74754032
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-13 01:31:03 +0300

    Feat: Created and implemented Order function in HTML.

--- src/app/component/order/order.component.ts
+++ src/app/component/order/order.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SharedModule } from '../../modules/shared.module';
 import { OrderPipe } from '../../pipes/order.pipe';
 import { RouterLink } from '@angular/router';
@@ -8,11 +8,14 @@ import { ProductModel } from '../../models/product.model';
 import { HttpService } from '../../sevices/http.service';
 import { SwalService } from '../../sevices/swal.service';
 import { DatePipe } from '@angular/common';
+import { OrderDetailModel } from '../../models/order-detail.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-order',
   standalone: true,
   imports: [SharedModule, OrderPipe],
+  providers: [DatePipe],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css',
 })
@@ -20,13 +23,21 @@ export class OrderComponent {
   orders: OrderModel[] = [];
   customers: CustomerModel[] = [];
   products: ProductModel[] = [];
+  createDetail: OrderDetailModel = new OrderDetailModel();
 
   search: string = '';
+
+  @ViewChild("createModalCloseBtn") createModalCloseBtn: ElementRef<HTMLButtonElement> | undefined;
+
+  createModel:OrderModel = new OrderModel();
   constructor(
     private http: HttpService,
     private swal: SwalService,
     private date: DatePipe
-  ) {}
+  ) {
+    this.createModel.date = this.date.transform(new Date(), "yyyy-MM-dd") ?? "";
+    this.createModel.deliveryDate = this.date.transform(new Date(), "yyyy-MM-dd") ?? "";
+  }
 
   ngOnInit(): void {
     this.getAll();
@@ -50,5 +61,33 @@ export class OrderComponent {
     this.http.post<CustomerModel[]>('Customers/GetAll', {}, (res) => {
       this.customers = res;
     });
+  }
+
+  addDetail(){
+    const product = this.products.find(p=> p.id == this.createDetail.productId);
+    if(product){
+      this.createDetail.product = product;
+    }
+
+    this.createModel.details.push(this.createDetail);
+    this.createDetail = new OrderDetailModel();
+  }
+
+  create(form: NgForm){
+    if(form.valid){
+      this.http.post<string>("Orders/Create",this.createModel,(res)=> {
+        this.swal.callToast(res);
+        this.createModel = new OrderModel();
+        this.createModel.date = this.date.transform(new Date(), "yyyy-MM-dd") ?? "";
+        this.createModel.deliveryDate = this.date.transform(new Date(), "yyyy-MM-dd") ?? "";
+
+        this.createModalCloseBtn?.nativeElement.click();
+        this.getAll();
+      });
+    }
+  }
+
+  removeDetail(index:number){
+    this.createModel.details.splice(index,1);
   }
 }
