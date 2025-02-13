59b1830649ac0366733fb1137c94dbf6c0a5c2b0
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-14 01:50:22 +0300

    Feat: Implemented setStatusClass function and integrated it into HTML.

--- src/app/component/order/order.component.ts
+++ src/app/component/order/order.component.ts
@@ -141,4 +141,10 @@ export class OrderComponent {
       });
     }
   }
+
+  setStatusClass(statusValue: number){
+    if(statusValue === 1) return "text-danger";
+    else if(statusValue === 2) return "text-primary";
+    else return "text-success";
+  }
 }
