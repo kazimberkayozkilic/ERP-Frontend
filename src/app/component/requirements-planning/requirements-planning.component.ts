ddfc54ebe83d7fae92d82c794127fa3d8b002f07
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-15 02:03:06 +0300

    feat: implemented data binding in RequirementsPlanningComponent HTML.

--- src/app/component/requirements-planning/requirements-planning.component.ts
+++ src/app/component/requirements-planning/requirements-planning.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RequirementsPlanningModel } from '../../models/requirements-planning.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-requirements-planning',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './requirements-planning.component.html',
   styleUrl: './requirements-planning.component.css'
 })
 export class RequirementsPlanningComponent {
-
+  data: RequirementsPlanningModel = new RequirementsPlanningModel();
+  orderId: string = "";
 }
