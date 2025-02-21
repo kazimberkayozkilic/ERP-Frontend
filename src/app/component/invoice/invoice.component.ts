1d1974ed20d761048422cb5d2a34275db6129780
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-22 01:03:19 +0300

    refactor: Simplified invoice routes by using a dynamic type parameter.

--- src/app/component/invoice/invoice.component.ts
+++ src/app/component/invoice/invoice.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../modules/shared.module';
+import { InvoiceModel } from '../../models/invoice.model';
+import { CustomerModel } from '../../models/customer.model';
+import { HttpService } from '../../sevices/http.service';
+import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
+import { SwalService } from '../../sevices/swal.service';
+import { DepotModel } from '../../models/depot.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-invoice',
@@ -9,5 +16,26 @@ import { SharedModule } from '../../modules/shared.module';
   styleUrl: './invoice.component.css'
 })
 export class InvoiceComponent {
+invoices: InvoiceModel[] = [];
+customer: CustomerModel[] = [];
+depots: DepotModel[]=[];
+search: string="";type: number = 1;
+typeName: string = "Alış";
+constructor(private http: HttpService, private swal: SwalService, private activated: ActivatedRoute) {
+  this.activated.params.subscribe(res=> {
+    this.type = res["type"] == "purchase" ? 1 : 2;
+    this.typeName = this.type == 1 ? "Alış" : "Satış";
+
+    this.getAll();
+  })
+
+}
+
+getAll(){
+  this.http.post<InvoiceModel[]>("Invoices/GetAll",{type: this.type},(res)=> {
+    this.invoices = res;
+  });
+}
+
 
 }
